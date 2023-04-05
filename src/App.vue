06da7abf55440ad4e8e9bae3db07940fdb0383bc
Alisa Zobova <zobovaalice@gmail.com>
2023-04-05 18:23:51 +0300

    feat: add menu for small devices

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,34 @@
   <v-app>
     <header class="page-header">
       <nav>
-        <v-spacer></v-spacer>
+        <v-spacer />
 
         <ul class="menu-links">
           <li class="menu-links__item"><a href="#">Home</a></li>
           <li class="menu-links__item"><a href="#">Featured collections</a></li>
           <li class="menu-links__item"><a href="#">My profile</a></li>
         </ul>
+
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-app-bar-nav-icon
+              class="menu-icon"
+              v-bind="attrs"
+              v-on="on"
+            ></v-app-bar-nav-icon>
+          </template>
+          <v-list class="menu-list">
+            <v-list-item class="menu-links__item">
+              <v-list-item-title> Home </v-list-item-title>
+            </v-list-item>
+            <v-list-item class="menu-links__item">
+              <v-list-item-title> Featured collections </v-list-item-title>
+            </v-list-item>
+            <v-list-item class="menu-links__item">
+              <v-list-item-title> My profile </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
 
         <ul class="menu-icons">
           <li class="menu-icons__item"><v-icon>mdi-account-outline</v-icon></li>
